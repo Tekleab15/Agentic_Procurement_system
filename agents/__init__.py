d468e3925f02cbbf3d5af59840df2129cdbdfad8
Tekleab15 <teklishgmn15@gmail.com>
2026-01-14 11:15:39 +0300

    Orchesterate agents to an Initializer

---
+++ agents/__init__.py
@@ -0,0 +1,6 @@
+from .base import AgentConfig
+from .extractor import ExtractorAgent
+from .verifier import VerifierAgent
+from .secops import SecOpsAgent
+
+__all__ = ["AgentConfig", "ExtractorAgent", "VerifierAgent", "SecOpsAgent"]
